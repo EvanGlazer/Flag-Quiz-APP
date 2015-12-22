02644b24da00c49579a8637e863c352536d2dcdd
unknown <evanowner@live.com>
2015-12-21 20:42:35 -0500

    UI Complete

---
+++ app/src/main/java/com/evanglazer/flagquizapp/SettingsActivity.java
@@ -0,0 +1,7 @@
+package com.evanglazer.flagquizapp;
+
+/**
+ * Created by Evan on 12/21/2015.
+ */
+public class SettingsActivity {
+}
---
+++ app/src/main/java/com/evanglazer/flagquizapp/SettingsFragment.java
@@ -0,0 +1,11 @@
+package com.evanglazer.flagquizapp;
+
+/**
+ * Created by Evan on 12/21/2015.
+ */
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+public class SettingsFragment {
+}
